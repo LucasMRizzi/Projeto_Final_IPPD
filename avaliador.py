282944f8f3d3892daa282ea6c51c33d7fd01e9a2
Lucas Rizzi <lucas.m.rizzi@gmail.com>
2025-11-18 21:38:06 -0300

    Fix: Limitação do processos mpirun com -np, substituído por --use-hwthread-cpus

--- avaliador.py
+++ avaliador.py
@@ -82,7 +82,7 @@ def run_benchmark(golden_checksum, args):
         base_cmd = [f"./{exe['output']}"] + args
         run_env = os.environ.copy()
         if exe['type'] == 'omp': run_env['OMP_NUM_THREADS'] = str(CPU_CORES)
-        elif exe['type'] == 'mpi': base_cmd = ["mpirun", "-np", str(CPU_CORES)] + base_cmd
+        elif exe['type'] == 'mpi': base_cmd = ["mpirun", "--use-hwthread-cpus"] + base_cmd
             
         for i in range(NUM_RUNS):
             print(f"  Execução {i + 1}/{NUM_RUNS}... ", end='', flush=True)
